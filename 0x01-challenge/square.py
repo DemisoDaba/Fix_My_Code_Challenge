a394a9d4e08c6ecfe3141eb745252030668cf271
DemisoDaba <demo.nkmt1@gmail.com>
2023-10-13 05:10:36 -0700

    Task1 and Task2

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Square Module
+"""
+
+
+class Square():
+    """Square class
+    """
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """init function
+        """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """permiter_of_my_square function
+        """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """str function
+        """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
